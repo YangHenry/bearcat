124b2697f040a1ac9cfc949e02233d7f0f5058b4
Advansil <marmortor@gmail.com>
2012-11-15 16:44:30 +0800

    1644

--- first1.php
+++ first1.php
@@ -2,33 +2,35 @@
 <body>
 
 <?php
-// set access parameters
-//$db = "D:\wamp\www\bearcat\users.db";
-$db="./users.db";
+$db = sqlite_open("db.sqlite");
 
-// open database file
-// make sure script has read/write permissions!
-$conn = sqlite_open('../users.db') or die ("ERROR: Cannot open database");
+// 如果你还需创建表foo,请将下面一句的注释标志清除.
+//sqlite_query($db , "CREATE TABLE foo (id INTEGER PRIMARY KEY, name CHAR(255))");
 
-// create and execute INSERT query
-$sql = "INSERT INTO users (id, username, country) VALUES (5, 'pierre', 'FR')";
-$result = sqlite_query($conn, $sql) or die("Error in query execution: " . sqlite_error_string(sqlite_last_error($conn)));
+//插入示例数据
+//sqlite_query($db, "INSERT INTO foo (name) VALUES ('Ilia4')");
+//sqlite_query($db, "INSERT INTO foo (name) VALUES ('Ilia5')");
+//sqlite_query($db, "INSERT INTO foo (name) VALUES ('Ilia6')");
 
-// create and execute SELECT query
-$sql = "SELECT username, country FROM users";
-$result = sqlite_query($conn, $sql) or die("Error in query execution: " . sqlite_error_string(sqlite_last_error($conn)));
-
-
-// check for returned rows
- //print if available
-if (sqlite_num_rows($result) > 0) {
-　　　while($row = sqlite_fetch_array($result)) {
-　　　　　　 echo $row[0] . " (" . $row[1] . ") ";
-　　　}
+// 执行查询
+$result = sqlite_query($db, "SELECT * FROM foo");
+// 迭代访问数据行
+while ($row = sqlite_fetch_array($result)) {
+    print_r($row);
+    print "<br />";
+    /*查询结果的每一行如下所示
+     Array
+     (
+         [0] => 1
+         [id] => 1
+         [1] => Ilia
+         [name] => Ilia
+     )
+*/
 }
 
-// close database file
-sqlite_close($conn);
+// 手动关闭数据连接
+sqlite_close($db); 
 ?>
 
 </body>
