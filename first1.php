5760f77a5de8ba3f8f83ef83ae43a40949cb9d0e
Advansil <marmortor@gmail.com>
2012-11-23 13:17:30 +0800

    11-23 1317

--- first1.php
+++ first1.php
@@ -2,35 +2,44 @@
 <body>
 
 <?php
-$db = sqlite_open("db.sqlite");
 
-// 如果你还需创建表foo,请将下面一句的注释标志清除.
-//sqlite_query($db , "CREATE TABLE foo (id INTEGER PRIMARY KEY, name CHAR(255))");
+try {
+	$db = new PDO('sqlite:test.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // 如果你还需创建表foo,请将下面一句的注释标志清除.
+    $db->exec('DROP TABLE IF EXISTS foo');
+    $db->exec('
+    CREATE TABLE foo(     
+    ID integer NOT NULL,
+    name char(255) NOT NULL,
+    PRIMARY KEY(ID)
+    )');     
+ } catch (PDOException $e) {
+    die("Error: " . $e->getMessage() . "<br />");
+    exit;     
+ }
 
-//插入示例数据
-//sqlite_query($db, "INSERT INTO foo (name) VALUES ('Ilia4')");
-//sqlite_query($db, "INSERT INTO foo (name) VALUES ('Ilia5')");
-//sqlite_query($db, "INSERT INTO foo (name) VALUES ('Ilia6')");
+// 插入示例数据
+echo $db->exec("INSERT INTO foo (name) VALUES ('Ilia1')");
+echo $db->exec("INSERT INTO foo (name) VALUES ('Ilia2')");
+echo $db->exec("INSERT INTO foo (name) VALUES ('Ilia3')");
+echo $db->exec("INSERT INTO foo (name) VALUES ('Ilia4')");
+echo $db->exec("INSERT INTO foo (name) VALUES ('Ilia5')");
+echo $db->exec("INSERT INTO foo (name) VALUES ('Ilia6')");
 
 // 执行查询
-$result = sqlite_query($db, "SELECT * FROM foo");
-// 迭代访问数据行
-while ($row = sqlite_fetch_array($result)) {
-    print_r($row);
-    print "<br />";
-    /*查询结果的每一行如下所示
-     Array
-     (
-         [0] => 1
-         [id] => 1
-         [1] => Ilia
-         [name] => Ilia
-     )
-*/
-}
+$result = $db->query("SELECT * FROM foo");
+$result->setFetchMode(PDO::FETCH_ASSOC);
+
+// 访问数据行
+$result_arr = $result->fetchAll();
+print_r($result_arr);
+
+$result_arr = NULL;
 
 // 手动关闭数据连接
-sqlite_close($db); 
+$db = NULL;
 ?>
 
 </body>
